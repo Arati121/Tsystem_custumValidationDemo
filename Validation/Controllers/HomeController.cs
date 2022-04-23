1e9fa34afce46bc493644292ff1fdfd1d3c6629c
Arati121 <avbhosale@mitaoe.ac.in>
2022-04-23 15:50:08 +0530

    Add project files.

---
+++ Validation/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
